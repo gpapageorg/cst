0954956b5179ede2ddec5f6f01f65df278f7bc2a
george <georg.papageorg@gmail.com>
2024-07-09 23:57:21 +0300

    changed logo

--- interaction.py
+++ interaction.py
@@ -3,7 +3,15 @@ from colors import *
 
 class Interaction:
     def __init__(self):
-        print(bcolors.HEADER + bcolors.BOLD + "Control Systems Tool" + bcolors.ENDC)
+        st = '''   _____               _                _    _____              _                          _______             _ 
+  / ____|             | |              | |  / ____|            | |                        |__   __|           | |
+ | |      ___   _ __  | |_  _ __  ___  | | | (___   _   _  ___ | |_  ___  _ __ ___   ___     | |  ___    ___  | |
+ | |     / _ \ | '_ \ | __|| '__|/ _ \ | |  \___ \ | | | |/ __|| __|/ _ \| '_ ` _ \ / __|    | | / _ \  / _ \ | |
+ | |____| (_) || | | || |_ | |  | (_) || |  ____) || |_| |\__ \| |_|  __/| | | | | |\__ \    | || (_) || (_) || |
+  \_____|\___/ |_| |_| \__||_|   \___/ |_| |_____/  \__, ||___/ \__|\___||_| |_| |_||___/    |_| \___/  \___/ |_|
+                                                     __/ |                                                       
+                                                    |___/                                                        '''
+        print(bcolors.HEADER + bcolors.BOLD + st + bcolors.ENDC)
         self.printAvailCommands()
     
     def printAvailCommands(self):
